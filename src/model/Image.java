6bd5cc506b6b6fc237bf203a13c87712ae0be1fa
tw-ilson <wilson.th@northeastern.edu>
2021-06-05 19:13:16 -0700

    Added an interface to represent images.

---
+++ src/model/Image.java
@@ -0,0 +1,27 @@
+package model;
+
+/**
+ * Represents an image as a width, a height and an array of pixels.
+ * NOTE: could add fields representing effects applied such as +1 blur +2 gray
+ */
+public interface Image {
+
+  /**
+   * Gets the array of this image's pixels.
+   * @return the array of this image's pixels
+   * @throws IllegalStateException if pixels cannot be produced
+   */
+  public int[] pixArray() throws IllegalStateException;
+
+  /**
+   * gets the widf of this image (in pixels).
+   * @return the width of this image
+   */
+  public int getWidth();
+
+  /**
+   * gets the height of this image (in pixels).
+   * @return the height of this image
+   */
+  public int getHeight();
+}
--- src/model/ImageProcessorIO.java
+++ src/model/ImageProcessorIO.java
@@ -11,12 +11,22 @@ public interface ImageProcessorIO {
    * Imports the specified image file to the model.
    * NOTE: is readable the right parameter type to use here? Change to void method?
    * @return message detailing sucessful import.
+   * @throws IllegalArgumentException if the file passed as argument is not supported.
    */
   public String importImage(Readable file) throws IllegalArgumentException;
 
   /**
    * Exports the stored image as a ???
+   * NOTE: possibly add enumerated argument to represent supported file formats.
    * @return the compiled image
+   * @throws IllegalStateException if the export requested is not possible
    */
   public String export() throws IllegalStateException;
+
+  /**
+   * Gets the current image state as an array of hexadecimal integers representing the pixels of the image
+   * @return pixel array
+   * @throws IllegalStateException if the pixels cannot be read from the current image
+   */
+  public int[] getImageState() throws IllegalStateException;
 }
