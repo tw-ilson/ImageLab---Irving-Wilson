3efa2fa6886c4eefc0598ba9c1689bf837d19215
tw-ilson <wilson.th@northeastern.edu>
2021-06-07 19:19:19 -0700

    Added JUnit to classpath

--- src/model/ColorUtils/Color.java
+++ src/model/ColorUtils/Color.java
@@ -2,18 +2,16 @@ package model.ColorUtils;
 
 public class Color {
   private int color;
-  private int r;
-  private int g;
-  private int b;
+
 
   public Color(int color) {
     this.color = color;
   }
 
   public Color(int r, int g, int b) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+    r = r << 16;
+    g = g << 8;
+    this.color = r & g & b;
   }
 
   public int getRed() {
---
+++ test/hw05/TestColor.java
@@ -0,0 +1,14 @@
+package hw05;
+
+import org.junit.Before;
+
+/**
+ * Tests the proper functionality of the color object and its methods.
+ */
+public class TestColor {
+
+  @Before
+  public void setup() {
+
+  }
+}
