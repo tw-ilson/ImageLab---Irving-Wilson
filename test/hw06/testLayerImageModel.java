a7dc75ac0ffa9c83ba46db653ffe3dc9ea4dd578
Lysander.curiosum <67127172+Lysander-curiosum@users.noreply.github.com>
2021-06-18 18:22:23 -0700

    SimpleLayeredImage

---
+++ test/hw06/testLayerImageModel.java
@@ -0,0 +1,14 @@
+package hw06;
+
+import model.ImageProcessorModel;
+import model.LayerImageModel;
+import org.junit.Before;
+
+public class testLayerImageModel {
+
+  @Before
+  public void init() {
+    ImageProcessorModel model = new LayerImageModel();
+
+  }
+}
